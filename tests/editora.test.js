66c81c77dd0263a0dc1e123f89cc4692d7098cd3
Deive Altoé <deivealtoe@gmail.com>
2022-04-06 23:14:00 -0300

    Adicionado teste para cadastrar e deletar uma editora

--- tests/editora.test.js
+++ tests/editora.test.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
 
 const buscarEditoraPorId = async (_id) => {
-  const resposta = await axios.get(`http://127.0.0.1:3000/editora/${_id}`, { validateStatus: () => true })
+  const resposta = await axios.get(`http://127.0.0.1:3000/editoras/${_id}`, { validateStatus: () => true })
 
   return resposta
+}
+
+const cadastrarEditora = async (novaEditora) => {
+  const resposta = await axios.post('http://127.0.0.1:3000/editoras', novaEditora)
+
+  return resposta
+}
+
+const deletarEditora = async (_id) => {
+    const resposta = await axios.delete(`http://127.0.0.1:3000/editoras/${_id}`)
+
+    return resposta
 }
 
 it('Deve retornar status 404 ao buscar editora com ID inexistente', async () => {
@@ -12,4 +24,19 @@ it('Deve retornar status 404 ao buscar editora com ID inexistente', async () => 
   const respostaGet = await buscarEditoraPorId(_id)
 
   expect(respostaGet.status).toBe(404)
+})
+
+it('Deve cadastrar uma editora e deletá-la', async () => {
+  const novaEditora = {
+    nome: "Nova Editora Jest"
+  }
+
+  const respostaCadastro = await cadastrarEditora(novaEditora)
+
+  expect(respostaCadastro.status).toBe(201)
+  expect(respostaCadastro.data.nome).toBe(novaEditora.nome)
+
+  const respostaDelete = await deletarEditora(respostaCadastro.data._id)
+
+  expect(respostaDelete.status).toBe(200)
 })
